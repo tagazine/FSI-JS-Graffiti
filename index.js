0e4a34aff46f67d08f136fecb0553c5ccd91f2c7
Your Namegit config --global user.name Your <you@example.com>
2022-01-26 18:51:26 -0500

    Practice

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'right'
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+dogNames[i].style.textAlign = 'left'
+}
